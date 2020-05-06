a94b6eadfa335f20d518fe274d47c12341268c7f
itsJoKr <krnjicj@gmail.com>
2020-05-06 12:19:16 +0200

    Remove equatable from BlocEvent
    It wasn't really necessary and this way we can use freezed for events.

--- lib/source/event/bloc_event.dart
+++ lib/source/event/bloc_event.dart
@@ -1,18 +1,5 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 
-/// Default bloc event to pass
-/// We needed this because of [RequestEvent], to allow more different
-/// events, all events for building a bloc need to extend [BlocEvent]
-///
-/// [BlocEvent] will handle [Equatable] and [props] and fill them with default
-/// data.
 @immutable
-abstract class BlocEvent<T> extends Equatable{
-  const BlocEvent([this._props]);
-
-  final List<T> _props;
-
-  @override
-  List<T> get props => _props ?? <T>[];
-}
+abstract class BlocEvent<T> { }
