2338a440cabfc4b48465067ea9979ee172663ffb
itsJoKr <krnjicj@gmail.com>
2020-05-06 16:34:21 +0200

    Remove RequestEvent class

--- lib/source/event/request_event.dart
+++ lib/source/event/request_event.dart
@@ -3,18 +3,8 @@ import 'package:flutter/material.dart';
 import 'bloc_event.dart';
 
 @immutable
-abstract class RequestEvent<T> extends BlocEvent<T>{
-  const RequestEvent([this._props]);
-
-  final List<T> _props;
-
-  @override
-  List<T> get props => _props ?? <T>[];
-}
-
-@immutable
-class MakeRequest<T> extends RequestEvent<T>{
-  const MakeRequest(this.request);
+class MakeRequest<T> extends BlocEvent<T> {
+  MakeRequest(this.request);
 
   final Future<T> request;
 }
